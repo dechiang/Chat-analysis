071886986a04b90e804066febcfb6e7024b9cc27
Dennis Chiang <dennis8654@gmail.com>
2022-02-24 22:44:42 +0800

    first edition

---
+++ chat.py
@@ -0,0 +1,34 @@
+# chat analysis
+
+def read_file(filename):
+	lines = []
+	with open(filename, 'r', encoding = 'utf-8-sig') as f:
+		for line in f:
+			lines.append(line.strip())
+	return lines
+
+def convert(lines):
+	new = []
+	persone = None # 把person 設成沒有值
+	for line in lines:
+		if line == 'Allen':
+			person = 'Allen'
+			continue
+		elif line == 'Tom':
+			person = 'Tom'
+			continue
+		if person: # 如果person有值
+			new.append(person + ': ' + line)
+	return new
+
+def write_file(filename, lines):
+	with open(filename, 'w') as f:
+		for line in lines:
+			f.write(line + '\n')
+
+def main():
+	lines = read_file('input.txt')
+	lines = convert(lines)
+	write_file('output.txt', lines)
+
+main()
